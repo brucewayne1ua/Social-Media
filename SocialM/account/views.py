a4e8c2beb0dba6ebbde36ee19f280d116650ef3e
Rustam Ohli <adamsonrustam@gmail.com>
2024-05-27 16:15:52 +0300

    Add profile models

--- SocialM/account/views.py
+++ SocialM/account/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth import authenticate, login
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
+from .models import Profile
 
 # Registration views
 
@@ -32,6 +33,27 @@ def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
-
-
 # Registration
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Создать новый объект пользователя,
+            # но пока не сохранять его
+            new_user = user_form.save(commit=False)
+            # Установить выбранный пароль
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Сохранить объект User
+            new_user.save()
+            # Создать профиль пользователя
+            Profile.objects.create(user=new_user)
+            return render(request,
+                          'account/register_done.html',
+                          {'new_user': new_user})
+    else:
+        user_form = UserRegistrationForm()
+    return render(request,
+                  'account/register.html',
+                  {'user_form': user_form})
